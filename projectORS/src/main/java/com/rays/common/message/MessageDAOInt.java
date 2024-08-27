f88ea45d192b07d67a046a7065df097968f6d4d6
Rachipaliwal24 <rachipaliwal@gmail.com>
2024-08-27 19:13:52 +0530

    msg

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * @author Rachi Paliwal
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
